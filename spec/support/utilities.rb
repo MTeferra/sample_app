8546285d24cd8fe652b6e5308dc072ebfca60eb8
Michael Teferra <mteferra11@gmail.com>
2013-01-26 20:18:37 -0800

    Add a full_title helper

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -3,3 +3,9 @@ RSpec::Matchers::define :have_title do |text|
     Capybara.string(page.body).has_selector?('title', text: text)
   end
 end
+
+RSpec::Matchers::define :have_no_title do |text|
+  match do |page|
+    Capybara.string(page.body).has_no_selector?('title', text: text)
+  end
+end
